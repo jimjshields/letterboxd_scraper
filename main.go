f4ae84a62355fc5017ffc4d42352c3becd86ee55
Jim Shields <jim.j.shields@gmail.com>
2020-11-10 18:05:15 -0500

    Refactor & try some concurrency

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"letterboxd_scraper/scraper"
 	"os"
 )
 
 func main() {
 	directorName := os.Args[1]
-	scraper.ScrapeDirector(directorName)
+	data := scraper.ScrapeDirector(directorName)
+	for _, datum := range data {
+		fmt.Printf(datum.FilmName, datum.Name, datum.Price)
+	}
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -4,8 +4,33 @@ import (
 	"fmt"
 	"github.com/Jeffail/gabs/v2"
 	"github.com/gocolly/colly/v2"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+func ScrapeDirector(name string) []PriceEntry {
+	url := getDirectorUrl(name)
+	c := colly.NewCollector()
+	var data []PriceEntry
+	var filmChan = make(chan Film)
+	var servicesData = make([]PriceEntry, 0)
+
+	c.OnHTML("li.poster-container > div", func(e *colly.HTMLElement) {
+		go getFilm(e, filmChan)
+		// scrapeFilm(c, film.Url)
+		servicesData = append(servicesData, scrapeServices(<-filmChan)...)
+	})
+	err := c.Visit(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.Wait()
+	for _, priceEntry := range servicesData {
+		fmt.Println(priceEntry.FilmName, priceEntry.Name, priceEntry.Price)
+	}
+	return data
+}
 
 func parseJson(e *colly.Response) *gabs.Container {
 	jsonParsed, err := gabs.ParseJSON(e.Body)
@@ -24,38 +49,60 @@ type Film struct {
 }
 
 type PriceEntry struct {
-	Name     string
-	Format   string
-	Price    string
-	PriceType     string
-	FilmName string
-	FilmId   string
-	Url      string
+	Name      string
+	Format    string
+	Price     float64
+	PriceType string
+	FilmName  string
+	FilmId    string
+	Url       string
 }
 
 func getServicesData(jsonParsed *gabs.Container, film Film) []PriceEntry {
 	rent := jsonParsed.Path("best.rent")
 	streaming := jsonParsed.Path("best.stream")
 	prices := make([]PriceEntry, 0)
-	prices = append(prices, getPrices(film, "streaming", streaming)...)
-	prices = append(prices, getPrices(film, "rent", rent)...)
+	for _, price := range getPrices(film, "streaming", streaming) {
+		prices = append(prices, price)
+	}
+	for _, price := range getPrices(film, "rent", rent) {
+		prices = append(prices, price)
+	}
 	return prices
 }
 
 func getPrices(film Film, priceType string, prices *gabs.Container) []PriceEntry {
 	priceData := make([]PriceEntry, 0)
 	for _, child := range prices.Children() {
+		price := getPrice(child)
 		priceData = append(priceData, PriceEntry{
-			Name:     child.Path("name").String(),
-			Format:   child.Path("format").String(),
-			Price:    child.Path("price").String(),
-			PriceType:     priceType,
-			FilmName: film.Name,
-			FilmId:   film.Id,
-			Url:      film.ServicesUrl,
+			Name:      child.Path("name").String(),
+			Format:    child.Path("format").String(),
+			Price:     price,
+			PriceType: priceType,
+			FilmName:  film.Name,
+			FilmId:    film.Id,
+			Url:       film.ServicesUrl,
 		})
 	}
 	return priceData
+}
+
+var priceRegex = regexp.MustCompile(`\d+\.\d+`)
+
+func getPrice(child *gabs.Container) float64 {
+	priceString := child.Path("price").String()
+	var price float64
+	var err error
+	if !strings.Contains(priceString, "null") {
+		price, err = strconv.ParseFloat(string(priceRegex.Find([]byte(priceString))), 64)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		price = 0.0
+	}
+	return price
 }
 
 func getFilm(element *colly.HTMLElement, filmChan chan Film) {
@@ -79,39 +126,22 @@ func scrapeFilm(c *colly.Collector, url string) {
 	}
 }
 
-func scrapeServices(c *colly.Collector, filmDataChan chan []PriceEntry, film Film) {
+func scrapeServices(film Film) []PriceEntry {
+	c := colly.NewCollector()
+	prices := make([]PriceEntry, 0)
 	c.OnResponse(func(e *colly.Response) {
 		jsonParsed := parseJson(e)
-		prices := getServicesData(jsonParsed, film)
-		filmDataChan <- prices
+		prices = getServicesData(jsonParsed, film)
 	})
 	err := c.Visit(film.ServicesUrl)
 	if err != nil {
 		panic(err)
 	}
+	return prices
 }
 
-func ScrapeDirector(name string) {
+func getDirectorUrl(name string) string {
 	directorNameFmt := strings.Join(strings.Split(strings.ToLower(name), " "), "-")
 	url := fmt.Sprintf("https://www.letterboxd.com/director/%s", directorNameFmt)
-	fmt.Print(url)
-	c := colly.NewCollector()
-	filmChan := make(chan Film)
-	filmDataChan := make(chan []PriceEntry)
-
-	c.OnHTML("li.poster-container > div", func(e *colly.HTMLElement) {
-		go getFilm(e, filmChan)
-		// scrapeFilm(c, film.Url)
-		go scrapeServices(c, filmDataChan, <-filmChan)
-		data := <-filmDataChan
-		for _, i2 := range data {
-			fmt.Println(i2)
-		}
-	})
-	err := c.Visit(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	c.Wait()
+	return url
 }
